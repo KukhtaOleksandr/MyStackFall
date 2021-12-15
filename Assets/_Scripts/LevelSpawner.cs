a48240e44d42adf15e1e05b019dc954b6570b4f2
Саша <gsh.org48@gmail.com>
2021-12-15 17:05:12 +0200

    Created Level Spawner

---
+++ Assets/_Scripts/LevelSpawner.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] models;
+    [SerializeField] private GameObject winningModel;
+    private GameObject[] currentLevelModels = new GameObject[4];
+    private GameObject temporaryModel, temporaryWinningModel;
+    private int currentLevel = 1,currentLevelAddon=7;
+    private float i=0;
+
+    private void Start()
+    {
+        //Selecting all models from inspector
+        ModelSelection();
+        //Instantiating different difficulty models
+        for (i = 0; i > -currentLevel-currentLevelAddon; i-=0.5f)
+        {
+            if (currentLevel <= 20)
+                temporaryModel = Instantiate(currentLevelModels[Random.Range(0, 2)]);
+            if (currentLevel > 20 && currentLevel <= 50)
+                temporaryModel = Instantiate(currentLevelModels[Random.Range(1, 3)]);
+            if (currentLevel > 50 && currentLevel <= 100)
+                temporaryModel = Instantiate(currentLevelModels[Random.Range(2, 4)]);
+            if (currentLevel > 100)
+                temporaryModel = Instantiate(currentLevelModels[Random.Range(3, 4)]);
+
+            temporaryModel.transform.position = new Vector3(0, i - 0.01f, 0);
+            temporaryModel.transform.eulerAngles = new Vector3(0, i * 8, 0);
+        }
+        temporaryWinningModel=Instantiate(winningModel);
+        temporaryWinningModel.transform.position = new Vector3(0, i - 0.01f, 0);
+    }
+    private void ModelSelection()
+    {
+        int randomModel=Random.Range(0,5);
+
+        switch(randomModel)
+        {
+            case 0: 
+            for(int i=0;i<4;i++)
+            {
+                currentLevelModels[i]=models[i];
+            }
+            break;
+            case 1: 
+            for(int i=0;i<4;i++)
+            {
+                currentLevelModels[i]=models[i+4];
+            }
+            break;
+            case 2: 
+            for(int i=0;i<4;i++)
+            {
+                currentLevelModels[i]=models[i+8];
+            }
+            break;
+            case 3: 
+            for(int i=0;i<4;i++)
+            {
+                currentLevelModels[i]=models[i+12];
+            }
+            break;
+            case 4: 
+            for(int i=0;i<4;i++)
+            {
+                currentLevelModels[i]=models[i+16];
+            }
+            break;
+        }
+
+    }
+}
---
+++ Assets/_Scripts/PlayerScritps/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    private int movingDownSpeed = -600, movingUpSpeed = 600, maxUpDistance = 6;
+    private PlayerInput playerInputSystem;
+    private Rigidbody rigidBody;
+    private bool isHoldingTouch, checkForCollisionExit;
+
+    private void Awake()
+    {
+        playerInputSystem = new PlayerInput();
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    private void OnEnable()
+    {
+        playerInputSystem.Enable();
+    }
+    private void OnDisable()
+    {
+        playerInputSystem.Disable();
+    }
+    private void Start()
+    {
+        playerInputSystem.Player.Click.performed += _ => StartedTouching();
+        playerInputSystem.Player.Click.canceled += _ => CancelledTouching();
+
+    }
+
+    private void FixedUpdate()
+    {
+        //if max up position is reached then we start mowing ball down
+        if (rigidBody.velocity.y > maxUpDistance)
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, maxUpDistance, rigidBody.velocity.z);
+        //if player doesn't hold touch anymore then we start moving ball up
+        CheckForCollisionExit();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!isHoldingTouch)
+        {
+            rigidBody.velocity = new Vector3(0, movingUpSpeed * Time.fixedDeltaTime, 0);
+        }
+        else
+        {
+            checkForCollisionExit = true;
+        }
+    }
+    private void CheckForCollisionExit()
+    {
+        if (checkForCollisionExit)
+        {
+            if (!isHoldingTouch)
+            {
+                rigidBody.velocity = new Vector3(0, movingUpSpeed * Time.fixedDeltaTime, 0);
+                checkForCollisionExit = false;
+            }
+        }
+    }
+
+    private void StartedTouching()
+    {
+        isHoldingTouch = true;
+        rigidBody.velocity = new Vector3(0, movingDownSpeed * Time.fixedDeltaTime, 0);
+    }
+    private void CancelledTouching()
+    {
+        isHoldingTouch = false;
+    }
+
+
+}
