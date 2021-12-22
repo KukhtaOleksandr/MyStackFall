e19a2b346df1d0fe6a1903240870013ec559541c
Саша <gsh.org48@gmail.com>
2021-12-22 16:26:30 +0200

    Game ui

--- Assets/_Scripts/LevelScripts/Level.cs
+++ Assets/_Scripts/LevelScripts/Level.cs
@@ -5,7 +5,7 @@ public class Level : MonoBehaviour
     public UnityEvent GoToNextLevel;
     public int CurrentLevel{get;set;}
     private void Awake() {
-        //PlayerPrefs.SetInt("Level",1);
+        PlayerPrefs.SetInt("Level",20);
         CurrentLevel=PlayerPrefs.GetInt("Level",1);
     }
     private void Start() {
